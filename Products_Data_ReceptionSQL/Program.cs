1b680e88390ca35e6a77a38879cf3c0e05f35bb2
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2019-02-19 16:01:09 +0200

    Visual studio file created.

---
+++ Products_Data_ReceptionSQL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products_Data_ReceptionSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
