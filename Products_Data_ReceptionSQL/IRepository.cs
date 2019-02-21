989a803da6174dc299fc7250e6956df77f101b4b
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2019-02-21 15:48:19 +0200

    Interface IRepository created.

---
+++ Products_Data_ReceptionSQL/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products_Data_ReceptionSQL
+{
+    public interface IRepository<T>
+    {
+        T Get(int id);
+        List<T> GetAll();
+        void Update(T item);
+        void Add(T item);
+        void Delete(int id);
+    }
+}
