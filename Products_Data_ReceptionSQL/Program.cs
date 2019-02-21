58651b5eb61851298b5fc3b12fdc010441dfa281
DESKTOP-6KJ0KT3\Jolita <jolita.jezukeviciute@gmail.com>
2019-02-21 14:53:31 +0200

    It is good to only get string values.

--- Products_Data_ReceptionSQL/Program.cs
+++ Products_Data_ReceptionSQL/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,22 @@ namespace Products_Data_ReceptionSQL
     {
         static void Main(string[] args)
         {
+            SqlConnection con = new SqlConnection("Server=(localdb)\\MSSQLLocalDB;Database=VegianProducts");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM VeganProducts", con);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+             while(reader.Read())
+            {
+                Console.WriteLine($"{reader.GetSqlInt32(0)}, {reader.GetString(1)}, {reader.GetSqlSingle(2)}");
+            }
+            reader.Close();
+            con.Close();
+
+            if(Debugger.IsAttached)
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
